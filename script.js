633e07206deba9f36473b098d41a42b777963312
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2023-07-23 22:48:34 -0500

    fixed validation issues with phone requirement

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const errorDisplayers = document.querySelectorAll('.error'),
     password = document.getElementById('password'),
     passwordConfirm = document.getElementById('password-confirm'),
     inputFields = document.querySelectorAll('input'),
+    phone = document.getElementById('tel-number'),
     submitBtn = document.getElementById('submit-button');
 
 let count = 2;
@@ -24,22 +25,35 @@ for(let i=0; i<inputFields.length; i++){
     })
 }
 
+phone.addEventListener('keyup', (e)=>{
+    let message = errorDisplayers[3]
+    e.target.value == e.target.value.replace(/\D/g,'') ? onValidation(message, '', 1) : onValidation(message, '*Please enter valid number', 0)
+})
+
 email.addEventListener('keyup', (e) => {
     let message = errorDisplayers[2]
     email.value.includes('@') & email.value.includes('.com') ? onValidation(message, '', 1) : onValidation(message, 'Please provide a valid Email *', 0)
 })
 
 password.addEventListener('keyup', (e) => {
-    let message = errorDisplayers[3]
+    let message = errorDisplayers[4]
     password.value.length >= 8 ? onValidation(message, '', 1) : onValidation(message, 'Password requires minimum 8 charecters *', 0)
 })
 
 passwordConfirm.addEventListener('keyup', (e) => {
-    let message = errorDisplayers[4]
+    let message = errorDisplayers[5]
     password.value === e.target.value ? onValidation(message, '', 1) : onValidation(message, 'Password did not match *', 0)
 })
 
 submitBtn.addEventListener('click', (e)=>{
-    e.preventDefault();
+    e.preventDefault()
+    if(count > 5){
+        cardContainer.style.display = 'none'
+        outroOverlay.classList.remove('disabled')
+    }
+    else{
+        for(let i=0; i<errorDisplayers.length; i++){
+            errorDisplayers[i].textContent = '*This field is Required'
+        }
+    }
 })
-console.log(count)
